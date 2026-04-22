68f3454090069996e3a17a01e31f4069b8e90645
loz2754 <loz2754@gmail.com>
2026-04-22 15:56:24 +0100

    Bug fix

--- AUNManualAutoTextSwitch.py
+++ AUNManualAutoTextSwitch.py
@@ -10,17 +10,17 @@ class AUNManualAutoTextSwitch:
                         'name_mode': ("BOOLEAN", {"default": False, "label_on": "Manual", "label_off": "Auto", "tooltip": "Switch between 'Auto' (uses Filename) and 'Manual' (uses ManualName)."}),
                         }}
                 
-    RETURN_TYPES = ('STRING', 'BOOLEAN')
-    RETURN_NAMES = ('Filename', 'Name Mode',)
+    RETURN_TYPES = ('STRING', 'STRING', 'BOOLEAN')
+    RETURN_NAMES = ('Filename', 'ManualName', 'Name Mode',)
     FUNCTION = 'output'
     CATEGORY = 'AUN Nodes/Text'
     DESCRIPTION = "Switch between a manual name and an automatic filename for the output path. Returns the selected filename along with a boolean which can be used to switch other nodes."
     
-    def output(self, ManualName, name_mode, Filename=None):  
+    def output(self, Filename, ManualName, name_mode):
         if name_mode:
-            return ManualName, name_mode
+            return ManualName, ManualName, name_mode
         else:
-            return Filename, name_mode                
+            return Filename, ManualName, name_mode
     
 NODE_CLASS_MAPPINGS = {
     "AUNManualAutoTextSwitch": AUNManualAutoTextSwitch,
